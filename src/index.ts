64323e18a78781fa75dd84690e684610c5709e5e
Urmade <tobias-urban@outlook.com>
2020-03-24 17:13:04 +0100

    Added "npm install" to PR-Test action  Added new coding styles

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import bodyParser from "body-parser";
 
 import config from "./config";
 import loginApi from "./controller/auth/authApi";
-import {userCache} from "./controller/cache/cache";
+import { userCache } from "./controller/cache/cache";
 import { _User } from "./models/database/user";
 import apiRouter from "./controller/api/router";
 import siteRouter from "./views/router";
@@ -16,21 +16,21 @@ app.use(express.static(path.join(__dirname, "..", "static")));
 app.set("view engine", "ejs");
 
 app.use(session({
-		secret: config.cookieSecret,
-		resave: false,
-		saveUninitialized: true
-	}));
+    secret: config.cookieSecret,
+    resave: false,
+    saveUninitialized: true
+}));
 
 app.use("/auth", loginApi);
 
 app.use(async (req, res, next) => {
-	if (req.session && req.session.session) {
-		if (await userCache.get(req.session.session)) {
-			next();
-			return;
-		}
-	}
-	res.redirect("/auth/login?origin=" + req.originalUrl);
+    if (req.session && req.session.session) {
+        if (await userCache.get(req.session.session)) {
+            next();
+            return;
+        }
+    }
+    res.redirect("/auth/login?origin=" + req.originalUrl);
 })
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -42,5 +42,5 @@ app.use("/", siteRouter);
 
 
 app.listen(process.env.PORT || 8000, () => {
-	console.info("Server is running! (Default port:8000)");
+    console.info("Server is running! (Default port:8000)");
 })
