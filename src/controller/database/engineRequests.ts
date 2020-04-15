3e8b8718142ec28710aa25b638d8792b731df81e
Urmade <tobias-urban@outlook.com>
2020-04-15 23:03:11 +0200

    Added database models

--- src/controller/database/engineRequests.ts
+++ src/controller/database/engineRequests.ts
@@ -1,47 +1,48 @@
 import request, { UrlOptions, CoreOptions } from "request";
 import { getSystemToken } from "../auth/authUtil";
+import { _PeopleEngineUser, _PeopleEngineCampus, _PeopleEngineHub } from "../../models/database/engines";
 
 
 
 export abstract class PeopleEngine {
     private static baseUrl:string = "https://commasto-api-dev.azurewebsites.net/api/";
 
-    static async getAllUsers(token:string):Promise<Array<{[key:string]:any}>> {
+    static async getAllUsers(token:string):Promise<Array<_PeopleEngineUser>> {
         return this.requestEngine("users",token);
     }
-    static async createUser(token:string,body:{[key:string]:any}):Promise<{[key:string]:any}> {
+    static async createUser(token:string,body:{[key:string]:any}):Promise<_PeopleEngineUser> {
         return new Promise(async resolve => {
             let user = await this.requestEngineItem("users",token,"POST",body);
             resolve(user);
         })
     }
-    static async getCurrentUser(token:string):Promise<{[key:string]:any}> {
+    static async getCurrentUser(token:string):Promise<_PeopleEngineUser> {
         return this.requestEngineItem("users/current",token);
     }
-    static async getCurrentUserCampus(token:string):Promise<{[key:string]:any}> {
-        return this.requestEngine("hubs/campus/my",token);
+    static async getCurrentUserCampus(token:string):Promise<_PeopleEngineCampus> {
+        return this.requestEngineItem("hubs/campus/my",token);
     }
-    static async getCurrentUserHub(token:string):Promise<{[key:string]:any}> {
-        return this.requestEngine("hubs/my",token);
+    static async getCurrentUserHub(token:string):Promise<_PeopleEngineHub> {
+        return this.requestEngineItem("hubs/my",token);
     }
 
-    static async getAllHubs(token:string):Promise<Array<{[key:string]:any}>> {
+    static async getAllHubs(token:string):Promise<Array<_PeopleEngineHub>> {
         return this.requestEngine("hubs",token);
     }
-    static async getHub(token:string, hubId:string):Promise<{[key:string]:any}> {
-        return this.requestEngine("hubs/"+hubId,token);
+    static async getHub(token:string, hubId:string):Promise<_PeopleEngineHub> {
+        return this.requestEngineItem("hubs/"+hubId,token);
     }
-    static async getAllCampusInHub(token:string,hubId:string):Promise<Array<{[key:string]:any}>> {
+    static async getAllCampusInHub(token:string,hubId:string):Promise<Array<_PeopleEngineCampus>> {
         return this.requestEngine("hubs/"+hubId+"/campus",token);
     }
 
-    static async getAllCampus(token:string):Promise<Array<{[key:string]:any}>> {
+    static async getAllCampus(token:string):Promise<Array<_PeopleEngineCampus>> {
         return this.requestEngine("hubs/campus",token);
     }
-    static async getCampus(token:string, hubId:string, campusId:string):Promise<{[key:string]:any}> {
-        return this.requestEngine("hubs/"+hubId+"/campus/"+campusId,token);
+    static async getCampus(token:string, hubId:string, campusId:string):Promise<_PeopleEngineCampus> {
+        return this.requestEngineItem("hubs/"+hubId+"/campus/"+campusId,token);
     }
-    static async getAllCampusMembers(token:string, hubId:string, campusId:string):Promise<Array<{[key:string]:any}>> {
+    static async getAllCampusMembers(token:string, hubId:string, campusId:string):Promise<Array<_PeopleEngineUser>> {
         return this.requestEngine("hubs/"+hubId+"/campus/"+campusId+"/users",token);
     }
 
@@ -49,13 +50,13 @@ export abstract class PeopleEngine {
 
 
 
-    private static async requestEngineItem(database:string, token?:string, method?:string, body?:{[key:string]:any}):Promise<{[key:string]:any}> {
+    private static async requestEngineItem(database:string, token?:string, method?:string, body?:{[key:string]:any}):Promise<any> {
         return new Promise(async resolve => {
             resolve((await this.requestEngine(database,token,method,body))[0]);
         })
 
     }
-    private static async requestEngine(database:string, token?:string, method?:string, body?:{[key:string]:any}):Promise<Array<{[key:string]:any}>> {
+    private static async requestEngine(database:string, token?:string, method?:string, body?:{[key:string]:any}):Promise<Array<any>> {
         return new Promise(async resolve => {
             const endpoint = this.baseUrl + database;
 
